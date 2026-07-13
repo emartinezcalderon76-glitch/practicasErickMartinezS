0ec6397f58e94b5202fdc69ca194e06f52b2a832
ErickS <emartinezcalderon76@gmail.com>
2026-07-13 16:36:51 -0600

    practica de js

--- practica0JS/js/script.js
+++ practica0JS/js/script.js
@@ -1,5 +1,18 @@
-alert("Hola soy la nueva alerta");
+/*alert("Hola soy la nueva alerta");
 
 document.write("Hola soy un mensaje en el documento");
 
-console.log("Hola soy un mensaje en la consola");
+console.log("Hola soy un mensaje en la consola");*/
+
+/*let nom = document.getElementById("jose");
+nom.innerHTML = "Hola";*/
+
+function comprar (){
+    let saldo = 90;
+    let precio = 100;
+
+    let parrafoAlerta = document.getElementById("alerta");
+    parrafoAlerta.innerHTML = "Compra exitosa";
+
+    parrafoAlerta.classList.add("exito");
+}
